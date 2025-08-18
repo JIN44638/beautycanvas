b9af9ec84efdcb161564d67f208b80b9c5f4c336
JIN44638 <hoyajs77@gmail.com>
2025-08-18 18:13:11 +0900

    배너 작성중

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@ window.addEventListener("load", function () {
   // 비주얼 스와이퍼
   const visual = new Swiper(".visual", {
     // 클릭안하고 슬라이드 자동으로 넘어가게 설정
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
+    // autoplay: {
+    //   delay: 2500,
+    //   disableOnInteraction: false,
     },
     //마지막 슬라이드에서 loop 반복되게 설정 , 아래 코드는 스와이퍼 사이트에서 가져옴(pagination,navigation)
     loop: true,
@@ -19,7 +19,7 @@ window.addEventListener("load", function () {
   });
   //  상품 스와이퍼 적용 slidesPerView(이미지1.5개나옴),paceBetween 간격넣기, breakpoints반응형코드 적용
   const itemSwiper = new Swiper(".itemSwiper", {
-     autoplay: {
+    autoplay: {
       delay: 2500,
       disableOnInteraction: false,
     },
@@ -38,12 +38,21 @@ window.addEventListener("load", function () {
         slidesPerView: 3,
       },
     },
-     pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-        renderBullet: function (index, className) {
-          return '<span class="' + className + '">' + (index + 1) + "</span>";
-        },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      renderBullet: function (index, className) {
+        return '<span class="' + className + '">' + (index + 1) + "</span>";
       },
+    },
+  });
+  // 햄버거 메뉴 클릭시
+  const hamburger = this.document.querySelector("#hamburger");
+  const fixMenus = this.document.querySelector(".fix-menu");
+  hamburger.addEventListener("click", function () {
+    fixMenus.classList.add("active");
+  });
+  hamburger.addEventListener("mouseleave", function () {
+    fixMenus.classList.remove("active");
   });
 });
