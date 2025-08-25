30a22aa0eabca4c23d187e63409847b8bdeb9136
JIN44638 <hoyajs77@gmail.com>
2025-08-25 16:34:44 +0900

    퀵메뉴 스크롤시 생성

--- js/main.js
+++ js/main.js
@@ -66,6 +66,15 @@ window.addEventListener("load", function () {
 
   // gotop버튼
   const goTopBtn = this.document.querySelector("#goTop");
+  this.window.addEventListener("scroll", function () {
+    // console.log("스크롤시작"); //스크롤 시작하면 콘솔에 "스크롤시작" 글자찍힘
+    //스크롤값이 100보다 크면
+    if (this.document.documentElement.scrollTop > 100) {
+      goTopBtn.style.display = "block";
+    }else{
+         goTopBtn.style.display = "none";
+    }
+  });
   goTopBtn.addEventListener("click", function () {
     // 스크롤to는 어디로 가라는 의미 = 윈도위에서 맨위로 올리기(행동 스무스하게)
     window.scrollTo({
